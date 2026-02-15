07a8eafd7859d9924d76022a60126980cd61e2f4
poohcom1 <pooh677@gmail.com>
2026-02-15 21:00:07 +0700

    Revert "feat: add tab navigation"
    This reverts commit 4cd764547b041b816b83f98cc40cf05d7b8bd048.

--- client/src/pages/sheetPage/components/cellEdit/TextEdit.tsx
+++ client/src/pages/sheetPage/components/cellEdit/TextEdit.tsx
@@ -1,11 +1,25 @@
+import { useEffect, useRef, useState } from "react";
 import styled from "styled-components";
+import { EditButton } from "../../../../components/EditButton";
 import { BaseInput } from "../../../../components/BaseInput";
-import { BaseTextEdit } from "./BaseTextEdit";
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  flex-grow: 1;
+  gap: 4px;
+  width: fix-content;
+`;
 
 const Input = styled(BaseInput)`
   padding: 0;
   min-width: 50px;
-  width: 100%;
+`;
+
+const CustomEditButton = styled(EditButton)`
+  margin-left: auto;
+  text-align: right;
+  min-width: 25px;
 `;
 
 interface Props {
@@ -14,47 +28,102 @@ interface Props {
 }
 
 export const TextEdit = ({ value, onChange }: Props) => {
-  const isLink = value?.startsWith("http://") || value?.startsWith("https://");
+  const [currentValue, setCurrentValue] = useState(value);
+  const [editing, setEditing] = useState(false);
+  const [displayWidth, setDisplayWidth] = useState<number | undefined>(
+    undefined,
+  );
+
+  useEffect(() => {
+    setCurrentValue(value);
+  }, [value]);
+
+  useEffect(() => {
+    setDisplayWidth(measureRef.current?.scrollWidth);
+  }, [currentValue]);
+
+  const containerRef = useRef<HTMLDivElement>(null);
+  const inputRef = useRef<HTMLInputElement>(null);
+  const saveRef = useRef<HTMLButtonElement>(null);
+
+  const measureRef = useRef<HTMLDivElement>(null);
+
+  const isLink =
+    currentValue?.startsWith("http://") || currentValue?.startsWith("https://");
+
+  const startEdit = () => {
+    setEditing(true);
+    setTimeout(() => inputRef.current?.select(), 50);
+  };
 
   return (
-    <BaseTextEdit
-      autoResize
-      value={value}
-      onChange={onChange}
-      displayComponent={({ value }) =>
-        isLink ? (
+    <Container ref={containerRef}>
+      <div
+        ref={measureRef}
+        style={{
+          ...(editing
+            ? {
+                position: "absolute",
+                opacity: "0",
+                pointerEvents: "none",
+                whiteSpace: "nowrap",
+              }
+            : {}),
+          margin: "0px",
+          padding: "8px",
+          border: "2px solid transparent",
+        }}
+      >
+        {!editing && isLink ? (
           <a href={value} target="_blank">
             {value}
           </a>
         ) : (
-          <span style={{ fontWeight: 600 }}>{value}</span>
-        )
-      }
-      inputComponent={({
-        currentValue,
-        setCurrentValue: onCurrentValueChange,
-        setEditing,
-        inputRef,
-        onBlur,
-      }) => (
-        <Input
-          tabIndex={0}
-          ref={inputRef}
-          value={currentValue}
-          onKeyDown={(e) => {
-            if (e.key === "Enter") {
-              onCurrentValueChange?.(currentValue ?? "");
-              setEditing(false);
-              inputRef.current?.blur();
-            } else if (e.key === "Escape") {
-              setEditing(false);
-              inputRef.current?.blur();
-            }
-          }}
-          onChange={(e) => onCurrentValueChange?.(e.target.value)}
-          onBlur={onBlur}
-        />
-      )}
-    />
+          <span style={{ fontWeight: 600 }}>
+            {editing ? currentValue : value}
+          </span>
+        )}
+      </div>
+      <Input
+        style={{ width: displayWidth + "px" }}
+        hidden={!editing}
+        ref={inputRef}
+        value={currentValue}
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            onChange?.(currentValue ?? "");
+            setEditing(false);
+            inputRef.current?.blur();
+          } else if (e.key === "Escape") {
+            setEditing(false);
+            inputRef.current?.blur();
+          }
+        }}
+        onChange={(e) => {
+          setCurrentValue(e.target.value);
+        }}
+        onBlur={(e) => {
+          if (e.relatedTarget === saveRef.current) {
+            return;
+          }
+          onChange?.(currentValue ?? "");
+          setEditing(false);
+        }}
+      />
+      <CustomEditButton
+        ref={saveRef}
+        onClick={() => {
+          if (!editing) {
+            startEdit();
+          } else {
+            onChange?.(currentValue ?? "");
+            setEditing(false);
+            inputRef.current?.blur();
+          }
+        }}
+      >
+        {!editing ? "edit" : "save"}
+      </CustomEditButton>
+    </Container>
   );
 };
